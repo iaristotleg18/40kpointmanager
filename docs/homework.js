35a1aef2ccbe119358d5ddbc300c3f7ac55773a6
SpencerLawrenceBrown <spencerxbrown@gmail.com>
2020-05-02 15:50:41 -0400

    add homework

---
+++ docs/homework.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+  // 1) Create the click function
+  // 2) In the click function, get the value of data-answer
+  // 3) Set the text of the <div id='theAnswer'> to the value from data-answer
+
+});
+  
