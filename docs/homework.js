32020b42cddb2858c4583189386b955a41f04587
Isaiah Glick <isaiahglick@Isaiahs-MacBook-Pro.local>
2020-05-03 14:04:11 -0400

    dunnit

--- docs/homework.js
+++ docs/homework.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 
-  // 1) Create the click function
-  // 2) In the click function, get the value of data-answer
-  // 3) Set the text of the <div id='theAnswer'> to the value from data-answer
+$("#theQuestion").on("click", function(event){
+  var answer = $(this).data("#theQuestion")
+  $("#theAnswer").text(answer);
 
 });
-  
+
+});
