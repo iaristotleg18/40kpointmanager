e6371536d34d7394cc8593a8a5c0726f9d66dca5
SpencerLawrenceBrown <spencerxbrown@gmail.com>
2020-05-05 13:49:57 -0400

    homework

--- docs/homework.js
+++ docs/homework.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
 
-$("#theQuestion").on("click", function(event){
-  var answer = $(this).data("answer")
-  $("#theAnswer").text(answer);
-  console.log(answer)
-});
+  // All the lonely people
+  var people = [
+    { id: 1, name: 'Tim', age: 27},
+    { id: 2, name: 'Tammy', age: 17},
+    { id: 3, name: 'Young Josh', age: 41},
+    { id: 4, name: 'Old Josh', age: 42},
+  ];
 
+  // 1) Create a forEach loop to loop over all the people in the array
+  // 2) For each person add an <li> to our <ol> element with their name and their age i.e "Tim, 27"
+  // 3) The <li> should also store the person's id in a data element called data-id.
 });
