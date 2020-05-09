c89501997edb1853bc37057fba24a9c4c3b38354
SpencerLawrenceBrown <spencerxbrown@gmail.com>
2020-05-09 08:40:18 -0400

    homework

--- docs/homework.js
+++ docs/homework.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
 
+  var favoritePizzaToppings = [
+    'Mushroom',    // Most favorite
+    'Sausage',
+    'Onions',
+    'Spinach',
+    'Pepperoni',
+    'Pineapple'    // Least favorite
+  ];
+
+  // Assignment:
+  // Write the correct topping to the correct number in the `My Favorite Toppings` list.
+  //
+  // Hints:
+  // 1. Use jquery to update the TEXT of the span with the correct ID.
+  // 2. My most favorite topping should go in spot #1. My least favorite topping should go in spot #6.
+  // 3. To access the correct topping for the right spot you will need to use the index of the item in the array.
+
 });
