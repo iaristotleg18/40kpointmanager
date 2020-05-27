eb50e952cb2aae3551c04aa186b725a8e72f8995
Isaiah Glick <isaiahglick@Isaiahs-MacBook-Pro.local>
2020-05-27 17:04:39 -0400

    failed

--- docs/homework.js
+++ docs/homework.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+var selectedWord;
+
   var options = {
     adjectives: ['old', 'funky', 'yellow', 'crossed', 'crazy', 'stuffed', 'abnormal', 'disrespectful', 'bearded', 'greedy', 'magical'],
     adverbs: ['awkwardly', 'exactly', 'speedily', 'foolishly', 'unexpectedly', 'bravely', 'madly', 'obnoxiously'],
@@ -8,35 +10,38 @@ $(document).ready(function() {
     gerunds: ['running', 'smashing', 'juggling', 'swimming', 'crying', 'smelling'],
     lengthsOfTime: ['annual', 'bi-weekly', 'monthly', 'hourly', 'once-per-millenium'],
     name: ['bob', 'june', 'pete', 'josh', 'chuck', 'mary', 'cindy'],
-    livingThing: ['person', 'dog', 'tree', 'large bird', 'skunk'],
+    noun: ['person', 'dog', 'tree', 'large bird', 'skunk'],
     verbs: ['run', 'party', 'fly', 'slide', 'dance'],
   }
 
   // In each jquery statement you need to use [] to access the words in the options object.
 
   //First adjective
-  $("#firstAdjective").text(CODE_HERE);
+  $("#firstAdjective").text(options['adjectives'])
+  $("#firstAdjective").on('click', (function(event){
+    $("firstAdjective").text(selectedWord)
+  })
   //Second adjective
-  $("#secondAdjective").text(CODE_HERE);
-  //Third adjective
-  $("#thirdAdjective").text(CODE_HERE);
-  //Fourth adjective
-  $("#fourthAdjective").text(CODE_HERE);
-  //Name
-  $("#name").text(CODE_HERE);
-  //Noun
-  $("#noun").text(CODE_HERE);
-  //Number
-  $("#number").text(CODE_HERE);
-  //Length of Time
-  $("#lengthOfTime").text(CODE_HERE);
-  //Gerund
-  $("#gerund").text(CODE_HERE);
-  //Verb
-  $("#verb").text(CODE_HERE);
-  //Facial Feature
-  $("#facialFeature").text(CODE_HERE);
-  //Catchphrase
-  $("#catchPhrase").text(CODE_HERE);
+  // $("#secondAdjective").text(CODE_HERE);
+  // //Third adjective
+  // $("#thirdAdjective").text(CODE_HERE);
+  // //Fourth adjective
+  // $("#fourthAdjective").text(CODE_HERE);
+  // //Name
+  // $("#name").text(CODE_HERE);
+  // //Noun
+  // $("#noun").text(CODE_HERE);
+  // //Number
+  // $("#number").text(CODE_HERE);
+  // //Length of Time
+  // $("#lengthOfTime").text(CODE_HERE);
+  // //Gerund
+  // $("#gerund").text(CODE_HERE);
+  // //Verb
+  // $("#verb").text(CODE_HERE);
+  // //Facial Feature
+  // $("#facialFeature").text(CODE_HERE);
+  // //Catchphrase
+  // $("#catchPhrase").text(CODE_HERE);
 
 });
