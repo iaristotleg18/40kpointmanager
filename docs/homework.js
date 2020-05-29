cd20e026e3e5363de540a125ac2dec0ea10fc233
SpencerLawrenceBrown <spencerxbrown@gmail.com>
2020-05-28 21:13:33 -0400

    homework

--- docs/homework.js
+++ docs/homework.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
 
+  // Gets the detachment type
+  var currentDetachType = $("#detachment_type").children("option:selected").val();
+  $("#detachment_type").change(function() {
+    currentDetachType = $(this).children("option:selected").val();
+  });
+
+
+  $("#detachment_form").submit(function(event){
+    event.preventDefault();
+    var detachmentType = currentDetachType;
+
+    // YOUR CODE HERE
+    var commandPoints; // Set this equal to the command points value in the input
+
+    console.log(detachmentType);
+
+    // Add an AJAX post request
+    // The request should add a detachment to the DB
+    //
+    //
+    // url: 'http://localhost:8080' + '/api/detachment',
+    // contentType: "application/json",
+    // data: should be the three different values needed to save a detachment
+  });
 
 });
