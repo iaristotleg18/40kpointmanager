a82e6d76f0e9a0af3effe62f12bdd15bbc6c407d
Isaiah Glick <isaiahglick@Isaiahs-MacBook-Pro.local>
2020-11-21 18:05:43 -0500

    did it in five seconds

--- docs/40k.js
+++ docs/40k.js
@@ -62,6 +62,8 @@ $("#listDetach").on('click', '.detachElement', function(){
   }).done(function(data){
     detachmentUnits = data
     updateDetachmentUnitList();
+    var detachData = [];
+    detachData.push(modelId)
     console.log("The Emperor expects his subjects to provide the maximum possible effort, but He does not care if one measly adept gives less.", detachmentUnits)
     console.log("There shalt be none before the Emperor, save the Emperor himself since the Emperor grows greater and more powerful by the day.", data)
   });
